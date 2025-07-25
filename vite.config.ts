a6259cb16001079ebc06b55a60c9400733f3d920
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-07-25 22:23:02 +0100

    chore: installs and configures vitest with react testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
@@ -5,4 +6,19 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // https://vitest.dev/config/#configuring-vitest
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./tests/setup.ts'],
+    globals: true,
+    coverage: {
+      include: ['**/components/*', '**/pages/*'],
+      thresholds: {
+        statements: 50,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+    },
+  },
 });
