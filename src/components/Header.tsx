00d82f459e5e73122ba28c8764ea9777f5dd9209
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-07-26 11:41:00 +0100

    feat(header): adds responsivity

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,12 @@ import Logo from '../assets/logo.svg';
 
 const Header = () => {
   return (
-    <header className="flex flex-row items-center justify-between px-4">
-      <img src={Logo} alt="logo" className="w-12 h-12" />
+    <header className="flex flex-row items-center justify-between mx-4 md:mx-8 lg:mx-[165px]">
+      <img src={Logo} alt="logo" className="w-12 h-12 md:w-16 md:h-16" />
       <a
         target="_blank"
         href="https://linkedin.com/"
-        className="rounded-full bg-[#030303] text-[#FFF7F0] px-6 pt-3 pb-4 text-[16px] leading-[150%]"
+        className="rounded-full bg-[#030303] text-[#FFF7F0] px-6 pt-3 pb-4 text-[16px] leading-[150%] md:py-4 md:px-12"
       >
         Free consultation
       </a>
