bca45a95be382baf38dc66b2c0d409c1966276c3
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-07-26 14:02:24 +0100

    chore(cards): adds relative images

---
+++ src/assets/cards/index.ts
@@ -0,0 +1,15 @@
+import CardDetails01 from './card-detail-01.svg';
+import CardDetails02 from './card-detail-02.svg';
+import CardDetails03 from './card-detail-03.svg';
+import CardDetails04 from './card-detail-04.svg';
+import CardDetails05 from './card-detail-05.svg';
+import CardDetails06 from './card-detail-06.svg';
+
+export {
+  CardDetails01,
+  CardDetails02,
+  CardDetails03,
+  CardDetails04,
+  CardDetails05,
+  CardDetails06,
+};
