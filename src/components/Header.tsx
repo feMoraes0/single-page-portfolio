dca1e5c63f88d80f591ca8d08dad77ede61032fb
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-08-08 15:22:46 +0100

    fix(Header): adds navigation tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,18 @@ import Logo from '../assets/logo.svg';
 
 const Header = () => {
   return (
-    <header className="flex flex-row items-center justify-between mx-4 md:mx-8 lg:mx-[165px]">
-      <img src={Logo} alt="logo" className="w-12 h-12 md:w-16 md:h-16" />
-      <a
-        target="_blank"
-        href="https://linkedin.com/"
-        rel="noopener noreferrer"
-        className="rounded-full bg-[#030303] text-[#FFF7F0] px-6 pt-3 pb-4 text-[16px] leading-[150%] md:py-4 md:px-12"
-      >
-        Free consultation
-      </a>
+    <header className="mx-4 md:mx-8 lg:mx-[165px]">
+      <nav className="flex flex-row items-center justify-between">
+        <img src={Logo} alt="logo" className="w-12 h-12 md:w-16 md:h-16" />
+        <a
+          target="_blank"
+          href="https://linkedin.com/"
+          rel="noopener noreferrer"
+          className="rounded-full bg-[#030303] text-[#FFF7F0] px-6 pt-3 pb-4 text-[16px] leading-[150%] md:py-4 md:px-12"
+        >
+          Free consultation
+        </a>
+      </nav>
     </header>
   );
 };
